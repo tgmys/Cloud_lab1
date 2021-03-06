bf75a6286ce8db3a2ac406462da0c77358b6f09c
tgmys <t.gmys@o2.pl>
2021-03-06 22:30:20 +0100

    Przekształcono klase main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Nazwa projektudf");
         System.out.println(System.getProperty("java.runtime.version"));
         System.out.println(System.getProperty("java.version"));
 
+        DEV2 k = new DEV2();
+        Dev1 t = new  Dev1();
+        System.out.println(t.author);
+        System.out.println(k.author);
     }
 }
