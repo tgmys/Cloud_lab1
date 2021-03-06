8294e178f2381ce993da5beb9e6dc13e260ba665
tgmys <t.gmys@o2.pl>
2021-03-06 21:07:33 +0100

    Nadano nazwe projektu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Nazwa projektu");
         System.out.println(System.getProperty("java.runtime.version"));
         System.out.println(System.getProperty("java.version"));
     }
